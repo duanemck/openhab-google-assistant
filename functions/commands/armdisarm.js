35c689998198d13a9107da2309ff41c0bf994eff
Duane McKibbin <duane@entelect.co.za>
2021-01-31 18:28:38 +0200

    Expanded SecuritySystem support: - Alarm levels - Status report with open zone reporting

--- functions/commands/armdisarm.js
+++ functions/commands/armdisarm.js
@@ -1,3 +1,4 @@
+const SecuritySystem = require('../devices/securitysystem.js');
 const DefaultCommand = require('./default.js');
 
 class ArmDisarm extends DefaultCommand {
@@ -11,16 +12,93 @@ class ArmDisarm extends DefaultCommand {
 
   static convertParamsToValue(params, _, device) {
     let arm = params.arm;
+    let armLevel = params.armLevel;
+
+    if (armLevel) {
+      return armLevel;
+    }
+
     if (this.isInverted(device)) {
       arm = !arm;
     }
     return arm ? 'ON' : 'OFF';
   }
 
+  static getItemName(item, _, params) {
+    return SecuritySystem.getMemberToSendArmCommand(item, params);
+  }
+
+  static requiresItem() {
+    return true;
+  }
+
   static getResponseStates(params) {
     return {
-      isArmed: params.arm
+      isArmed: params.arm,
+      armLevel: params.armLevel
     };
+  }
+
+  static shouldValidateStateChange() {
+    return true;
+  }
+
+  static validateStateChange(params, item, device) {
+    const members = SecuritySystem.getMembers(item);
+    const isCurrentlyArmed = members[SecuritySystem.armedMemberName].state === 'ON';
+    const currentLevel = members[SecuritySystem.armLevelMemberName].state;
+
+    if (params.armLevel) {
+      const alreadyArmedAtThisLevel = params.arm && isCurrentlyArmed && params.armLevel === currentLevel;
+      if (alreadyArmedAtThisLevel) {
+        return this.getErrorMessage(device, 'alreadyInState');
+      }
+      return;
+    }
+
+    if (params.arm && isCurrentlyArmed) {
+      return this.getErrorMessage(device, 'alreadyArmed');
+    }
+
+    if (!params.arm && !isCurrentlyArmed) {
+      return this.getErrorMessage(device, 'alreadyDisarmed');
+    }
+
+  }
+
+  static getErrorMessage(device, errorCode) {
+    return {
+      ids: [device.id],
+      status: 'ERROR',
+      errorCode
+    }
+  }
+
+  static checkUpdateFailed(params, item, device) {
+    const members = SecuritySystem.getMembers(item);
+    const isCurrentlyArmed = members[SecuritySystem.armedMemberName].state === 'ON';
+    const currentLevel = members[SecuritySystem.armLevelMemberName].state;
+
+    const armStatusSuccessful = params.arm === isCurrentlyArmed;
+    const armLevelSuccessful = params.armLevel ? params.armLevel === currentLevel : true;
+
+    if (armStatusSuccessful && armLevelSuccessful) {
+      return;
+    }
+
+    return this.getErrorMessage(device, params.arm ? 'armFailure' : 'disarmFailure');
+  }
+
+  static getNewState(params, item, device) {
+    const members = SecuritySystem.getMembers(item);
+    let response = {
+      online: true,
+      isArmed: members[SecuritySystem.armedMemberName].state === 'ON'
+    }
+    if (params.armLevel) {
+      response.armLevel = members[SecuritySystem.armLevelMemberName].state;
+    }
+    return response;
   }
 }
 
--- functions/devices/securitysystem.js
+++ functions/devices/securitysystem.js
@@ -1,27 +1,184 @@
 const DefaultDevice = require('./default.js');
 
+// TODO:
+// Option for pin on disarm only
+// Don't store the pin at Google in the customData
+
+const configLang = 'lang';
+const configOrdered = 'ordered';
+const configArmLevels = 'armLevels';
+
+const zoneTypeOpenClose = 'OpenClose';
+const zoneTypeMotion = 'Motion';
+
+const memberArmed = 'securitySystemArmed';
+const memberArmLevel = 'securitySystemArmLevel';
+const memberZone = 'securitySystemZone';
+const memberTrouble = 'securitySystemTrouble';
+const memberErrorCode = 'securitySystemTroubleCode';
+
+const supportedMembers = [
+  memberArmed,
+  memberArmLevel,
+  memberZone,
+  memberTrouble,
+  memberErrorCode
+];
+
+const errorNotSupported = 'notSupported';
+const errorDeviceOpen = 'deviceOpen';
+const errorMotionDetected = 'motionDetected';
+
+const defaultLanguage = 'en';
+
+const stateSwitchActive = 'ON';
+const stateContactActive = 'OPEN';
+
+const zoneStateActive = [stateSwitchActive, stateContactActive];
+
 class SecuritySystem extends DefaultDevice {
+
   static get type() {
     return 'action.devices.types.SECURITYSYSTEM';
   }
 
   static getTraits() {
-    return ['action.devices.traits.ArmDisarm'];
+    return [
+      'action.devices.traits.ArmDisarm',
+      'action.devices.traits.StatusReport'
   }
 
-  static get requiredItemTypes() {
-    return ['Switch'];
+  static get armedMemberName() {
+    return memberArmed;
+  }
+
+  static get armLevelMemberName() {
+    return memberArmLevel;
+  }
+
+  static getMemberToSendArmCommand(item, params) {
+    const members = this.getMembers(item);
+    if (params.armLevel) {
+      if (memberArmLevel in members) {
+        return members[memberArmLevel].name;
+      }
+    }
+    if (memberArmed in members) {
+      return members[memberArmed].name;
+    }
+    throw { statusCode: 400 };
+  }
+
+  static matchesItemType(item) {
+    return item.type === 'Group' && Object.keys(this.getMembers(item)).length > 0;
+  }
+
+  static getAttributes(item) {
+
+    //Group [armLevels="L1=Stay,L2=Away", lang="en", ordered=true]
+    //Zone [zoneType="OpenClose", blocking=true]
+    //Zone [zoneType="Motion", blocking=false]
+
+    const config = this.getConfig(item);
+    const ordered = configOrdered in config ? config.ordered : false;
+    const language = configLang in config ? config.lang : defaultLanguage;
+
+    let attributes = {
+      availableArmLevels: { levels: [], ordered: ordered },
+    };
+
+    if (configArmLevels in config) {
+      attributes.availableArmLevels.levels = config.armLevels
+        .split(',')
+        .map(level => level.split('='))
+        .map(([levelName, levelSynonym]) => {
+          return {
+            level_name: levelName,
+            level_values: [{
+              level_synonym: [levelSynonym],
+              lang: language
+            }]
+
+          }
+        });
+    }
+    return attributes;
+  }
+
+  static getMembers(item) {
+    const members = {
+      zones: []
+    };
+    if (item.members && item.members.length) {
+      item.members.forEach(member => {
+        if (member.metadata && member.metadata.ga) {
+          const memberType = supportedMembers.find(m => member.metadata.ga.value.toLowerCase() === m.toLowerCase());
+          if (memberType) {
+            const memberDetails = { name: member.name, state: member.state, config: this.getConfig(member) };
+            if (memberType === memberZone) {
+              members.zones.push(memberDetails);
+            } else {
+              members[memberType] = memberDetails;
+            }
+          }
+        }
+      });
+    }
+    return members;
   }
 
   static getState(item) {
-    let state = item.state === 'ON';
+    const members = this.getMembers(item);
+    let state = members[memberArmed].state === stateSwitchActive;
+    let armLevel = null;
+    if (state) {
+      armLevel = members[memberArmLevel].state;
+    }
+
     if (this.getConfig(item).inverted === true) {
       state = !state;
     }
+
     return {
-      isArmed: state
+      isArmed: state,
+      currentArmLevel: armLevel,
+      currentStatusReport: this.getStatusReport(item, members)
     };
   }
+
+  static getStatusReport(item, members) {
+    let report = [];
+
+    let isTrouble = members[memberTrouble].state === stateSwitchActive;
+
+    if (isTrouble) {
+      let troubleCode = members[memberErrorCode].state;
+      report.push({
+        blocking: false,
+        deviceTarget: item.name,
+        priority: 0,
+        statusCode: troubleCode
+      });
+    };
+
+    for (let zone of members.zones) {
+      if (zoneStateActive.includes(zone.state)) {
+        let code = errorNotSupported;
+        switch (zone.config.zoneType) {
+          case zoneTypeOpenClose: code = errorDeviceOpen; break;
+          case zoneTypeMotion: code = errorMotionDetected; break;
+        };
+        report.push({
+          blocking: zone.config.blocking === true,
+          deviceTarget: zone.name,
+          priority: 1,
+          statusCode: code
+        })
+      }
+    }
+    return report;
+  }
+
 }
 
 module.exports = SecuritySystem;
